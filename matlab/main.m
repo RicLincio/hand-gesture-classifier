a4f9798e9cd4ec8fd46210af245de43adce7c878
RicLincio <riccardo.lincetto@gmail.com>
2020-11-27 22:49:42 +0100

    Matlab pipeline tuning

--- matlab/main.m
+++ matlab/main.m
@@ -5,7 +5,7 @@
 % generate the RGBD data to be processed by the classifier.
 
 clear; close all; clc;
-execEnv = 'cpu';
+execEnv = 'gpu';
 addpath lib
 
 %% training data
@@ -18,15 +18,16 @@ load(fullfile('..','data','validation_set'))                               % siz
 
 % matlab supports 1 or 3 channel images, use (gray, depth, ~)
 trainSet = prepareSet(trainSet);                                           % size 28 x 28 x 3 x 8370
-trainLabel = categorical(trainLabel);                                      % size 28 x 28 x 3 x 1440
+trainLabel = categorical(trainLabel,[1,2,3],{'five' 'fist' 'ok'});                                      % size 28 x 28 x 3 x 1440
 valSet = prepareSet(valSet);
-valLabel = categorical(valLabel);
+valLabel = categorical(valLabel,[1,2,3],{'five' 'fist' 'ok'});
 
 % data augmentation
 imageAugmenter = imageDataAugmenter('RandRotation',[-180 180], ...
                                     'RandXTranslation',[-3 3], ...
                                     'RandYTranslation',[-3 3], ...
                                     'RandScale',[.8 1.1], ...
+                                    'RandXShear',[-20 20], ...
                                     'RandXReflection', true, ...
                                     'RandYReflection', true);
 augimds = augmentedImageDatastore(size(trainSet,1:3),trainSet,trainLabel, ...
@@ -48,10 +49,11 @@ testLabel = [1,1,1,1,2,2,2,3,3];
 
 % store data
 save(fullfile('..','data','test_set'),'testSet','testLabel')
+load(fullfile('..','data','test_set'),'testSet','testLabel')
 
 % reformat data
 testSet = prepareSet(testSet);                                             % size 28 x 28 x 3 x 9
-testLabel = categorical(testLabel);
+testLabel = categorical(testLabel,[1,2,3],{'five' 'fist' 'ok'});
 
 %% cnn model
 close all
@@ -64,12 +66,15 @@ params.validationFrequency = floor(size(trainLabel, 2) ...
                              / params.numValidationsPerEpoch);
 % network options
 options = trainingOptions('sgdm', ...
-    'ExecutionEnvironment',execEnv, ...
-    'MaxEpochs', 2, ...
+    'ExecutionEnvironment','auto', ...
+    'MaxEpochs', 20, ...
     'Shuffle', 'every-epoch', ...
     'ValidationData', {valSet, valLabel}, ...
     'ValidationFrequency', params.validationFrequency, ...
     'VerboseFrequency', params.validationFrequency, ...
+    'LearnRateSchedule','piecewise', ...
+    'LearnRateDropPeriod',1, ...
+    'LearnRateDropFactor',0.5, ...
     'Verbose', true, ...                                                   % display messages
     'Plots', 'training-progress');                                         % display plots
 
@@ -77,7 +82,7 @@ options = trainingOptions('sgdm', ...
 layers = [
     imageInputLayer(size(trainSet(:,:,:,1)),'Name','input')                % first input layer
 
-    convolution2dLayer(3,64,'Padding',1,'Name','conv_1')                   % conv layer
+    convolution2dLayer(3,32,'Padding',1,'Name','conv_1')                   % conv layer
     batchNormalizationLayer('Name','bn_1')                                 % normalize
     reluLayer('Name','relu_1')                                             % ReLU
     maxPooling2dLayer(2,'Stride',2,'Name','pool_1')                        % max pooling
@@ -104,11 +109,28 @@ net = trainNetwork(augimds,layers, options);
 
 %% Classify the new test set
 
-result.prob = predict(net, testSet, 'ExecutionEnvironment', execEnv);      % predict softmax probs
-[result.top, result.class] = max(result.prob,[],2);                        % classification index
-disp(categorical(result.class))
-disp(testLabel)
+result.class = classify(net, testSet, 'ExecutionEnvironment', 'auto');     % predict softmax probs
+subplot(2,1,1)
+confusionchart(testLabel,categorical(result.class))
 
-% 1 -> five
-% 2 -> fist
-% 3 -> ok
+% how sure is the network on what it is predicting?
+result.prob = predict(net, testSet, 'ExecutionEnvironment', 'auto');       % predict softmax probs
+subplot(2,1,2)
+bar(result.prob,'stacked')
+legend('five','fist','ok')
+
+%% Inspect visually the network layers
+
+layer = 'full_1';
+channels = 1:64;
+
+I = deepDreamImage(net,layer,channels, ...
+    'PyramidLevels',1, ...
+    'Verbose',0, ...
+    'ExecutionEnvironment','auto');
+
+figure
+for i = 1:64
+    subplot(8,8,i)
+    imshow(I(:,:,:,i))
+end
